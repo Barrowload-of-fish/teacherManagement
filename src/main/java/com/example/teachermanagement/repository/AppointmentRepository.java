ad3f1c55a8d28a5eb4a594210839081625a630d1
yuanananananan <115727144+yuanananananan@users.noreply.github.com>
2024-06-09 23:59:00 +0800

    Add files via upload

---
+++ src/main/java/com/example/teachermanagement/repository/AppointmentRepository.java
@@ -0,0 +1,15 @@
+package com.example.teachermanagement.repository;
+
+
+import com.example.teachermanagement.model.Appointment;
+import com.example.teachermanagement.model.Teacher;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+
+    List<Appointment> findByTeacherId(Long teacherId);
+
+}
